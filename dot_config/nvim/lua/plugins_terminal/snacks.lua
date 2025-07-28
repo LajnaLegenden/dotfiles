be2c0ad03ef7c516b0e99215c17750210ef096bf
Linus Jansson <34426335+LajnaLegenden@users.noreply.github.com.>
2025-07-28 21:54:09 +0200

    Update .config/nvim/lua/plugins_terminal/snacks.lua

--- dot_config/nvim/lua/plugins_terminal/snacks.lua
+++ dot_config/nvim/lua/plugins_terminal/snacks.lua
@@ -3,9 +3,6 @@ return {
   ---@type snacks.Config
   opts = {
     picker = {
-      -- your picker configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     }
   }
 }
