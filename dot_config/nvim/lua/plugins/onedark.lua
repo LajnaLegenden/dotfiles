be7a86d4de3190edee9e1ff7852a6a54ed5d1472
Linus Jansson <34426335+LajnaLegenden@users.noreply.github.com.>
2025-07-28 21:47:06 +0200

    Update .chezmoidata.yaml Update .config/nvim/lua/plugins/onedark.lua

--- dot_config/nvim/lua/plugins/onedark.lua
+++ dot_config/nvim/lua/plugins/onedark.lua
@@ -2,7 +2,14 @@ return {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
   config = function()
-    vim.cmd("colorscheme onedark")
+    vim.cmd([[
+augroup TransparentBackground
+autocmd!
+autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+augroup END
+]])
+vim.cmd("colorscheme onedark")
   end
 }
 
