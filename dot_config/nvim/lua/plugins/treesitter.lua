c07e49c07f0487490929b0c963c582c8fd667baa
Linus Jansson <34426335+LajnaLegenden@users.noreply.github.com.>
2025-08-02 18:33:06 +0200

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,18 @@
-return {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate", opts = {
-    ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "jsx",
-        "tsx",
-        "css",
-        "html",
-        "json",
-        "yaml",
-        "markdown",
-    },
-    auto_install = true,
+return {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    lazy = false,
+     config = function () 
+      local configs = require("nvim-treesitter.configs")
 
-}}
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          auto_install = true,
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+    
+  }
