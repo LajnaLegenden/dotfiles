a1763c01a5b8fa1c6c8c6a0bcbdd724631cce60b
Linus Jansson <34426335+LajnaLegenden@users.noreply.github.com.>
2025-08-19 22:18:08 +0200

    Update .chezmoidata.yaml Update .config/hypr/hyprland.conf Update .config/waybar/config.jsonc Update nvim/lazy-lock.json Add nvim/lua/config/ts-tools.lua Update nvim/lua/plugins/mason.lua Update nvim/lua/plugins_terminal/diagflow.lua Add nvim/lua/plugins_terminal/typescript.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,29 +1,14 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
+  "mason-org/mason.nvim",
   opts = {
     ensure_installed = {
       "lua_ls",
       "rust_analyzer",
-      "vtsls",
+      "tsserver",
+      "eslint-lsp"
     },
   },
   dependencies = {
-    { 
-      "mason-org/mason.nvim", 
-      opts = {
-        ensure_installed = {
-          -- Formatters
-          "stylua",
-          "prettier",
-          "eslint_d",
-          "biome",
-          "black",
-          "isort",
-          -- Linters (eslint_d already listed above)
-          -- Note: biomejs is the same as biome for Mason
-        },
-      }
-    },
     "neovim/nvim-lspconfig",
   },
 }
--- nvim/lua/plugins_terminal/diagflow.lua
+++ nvim/lua/plugins_terminal/diagflow.lua
@@ -1,12 +1,1 @@
-
-return {
-    'dgagn/diagflow.nvim',
-    event = 'LspAttach',
-    opts = {
-        show_borders = true,
-        format = function(diagnostic)
-            return '[' .. diagnostic.source .. '] ' .. diagnostic.message
-        end,
-        scope = 'line',
-    }
-}
+return {}
